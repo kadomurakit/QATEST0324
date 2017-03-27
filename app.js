97bac64ea2d9514b6dfc73e84eb409c232f66be2
圭人 角村 <kadomura88@gmail.com>
2017-03-26 19:52:25 -0500

    確認のため、変更.

--- app.js
+++ app.js
@@ -12,28 +12,6 @@ var bodyParser = require('body-parser');
 
 var request = require('request');
 
-app.post('/api', function(req, res) {
-  var options = {
-    method: 'POST',
-    uri: 'https://api.line.me/v2/bot/message/reply',
-    body: {
-      replyToken: req.body.events[0].replyToken,
-      messages: [{
-        type: "text",
-        text: req.body.events[0].message.text
-      }]
-    },
-    auth: {
-      bearer: 'SqHs6RnPOfkmIhGAz7O7vbUKemOqzcJ1XQAcea7aWmMiJIZC6aOswHTepCmRlnMUFVoR/Xo3ebfLOwpIWlQpxqZbquf5HZms+pEjLnhJ/IXZNkrwdxKdg0WajmWu2X4CS4+48Z2wTMAM7I0Md9ViZAdB04t89/1O/w1cDnyilFU='
-    },
-    json: true
-  };
-  request(options, function(err, res, body) {
-    console.log(JSON.stringify(res));
-  });
-  res.send('OK');
-});
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
